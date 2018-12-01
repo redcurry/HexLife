9dfa6b6e83421f769ef17538c8612d9885e5a514
Carlos Anderson <carlosjanderson@gmail.com>
2018-12-01 15:59:47 -0500

    Do basic drawing of the hex grid

---
+++ HexLife/HexLife/HexGameOfLife.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HexLife
+{
+    public class HexGameOfLife
+    {
+        public HexGrid Grid => CreateRandomGrid();
+
+        public HexGameOfLife()
+        {
+        }
+
+        private HexGrid CreateRandomGrid()
+        {
+            var grid = new HexGrid(100, 100);
+            var random = new Random();
+            for (var i = 0; i < 100; i++)
+                for (var j = 0; j < 100; j++)
+                    grid[i, j].IsAlive = random.NextDouble() < 0.5;
+            return grid;
+        }
+    }
+
+    public class HexGrid
+    {
+        private readonly HexCell[,] _grid;
+
+        public HexGrid(int rows, int cols)
+        {
+            Rows = rows;
+            Columns = cols;
+
+            _grid = new HexCell[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    _grid[i, j] = new HexCell();
+        }
+
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public HexCell this[int i, int j] => _grid[i, j];
+    }
+
+    public class HexCell
+    {
+        public bool IsAlive { get; set; }
+    }
+}
--- HexLife/HexLife/MainPage.xaml.cs
+++ HexLife/HexLife/MainPage.xaml.cs
@@ -8,11 +8,13 @@ namespace HexLife
 {
     public partial class MainPage : ContentPage
     {
-        private Random _random = new Random();
+        private readonly HexGameOfLife _gol;
 
         public MainPage()
         {
             InitializeComponent();
+
+            _gol = new HexGameOfLife();
 
             var timer = new Timer(1000);
             timer.Elapsed += (sender, args) => Canvas.InvalidateSurface();
@@ -21,14 +23,42 @@ namespace HexLife
 
         private void SKCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
+            var grid = _gol.Grid;
             var canvas = e.Surface.Canvas;
+            canvas.Clear();
+
+            for (var i = 0; i < grid.Rows; i++)
+            for (var j = 0; j < grid.Columns; j++)
+            {
+                if (grid[i, j].IsAlive)
+                    DrawCell(canvas, i, j);
+            }
+        }
+
+        private void DrawCell(SKCanvas canvas, int i, int j)
+        {
+            const int CellRadius = 8;
+
             var paint = new SKPaint
             {
-                Style = SKPaintStyle.Stroke,
-                Color = Color.FromRgb(_random.Next(10, 250), _random.Next(10, 250), _random.Next(10, 250)).ToSKColor(),
-                StrokeWidth = 25
+                Style = SKPaintStyle.Fill,
+                Color = Color.Red.ToSKColor(),
             };
-            canvas.DrawCircle(e.Info.Width / 2, e.Info.Height / 2, 100, paint);
+
+            float x, y;
+
+            if (j % 2 == 0)
+            {
+                x = i * 2 * CellRadius + CellRadius;
+                y = j * (float)Math.Sqrt(3) * CellRadius + CellRadius;
+            }
+            else
+            {
+                x = i * 2 * CellRadius + CellRadius + CellRadius;
+                y = j * (float)Math.Sqrt(3) * CellRadius + CellRadius;
+            }
+
+            canvas.DrawCircle(x, y, CellRadius, paint);
         }
     }
 }

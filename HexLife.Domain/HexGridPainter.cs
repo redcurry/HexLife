49e79207d6e90e92c2fca6b902633da3f3bf3c69
Carlos Anderson <carlosjanderson@gmail.com>
2018-12-08 14:47:58 -0500

    Show hex game of life on WPF windows

---
+++ HexLife.Domain/HexGridPainter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using SkiaSharp;
+
+namespace HexLife.Domain
+{
+    public class HexGridPainter
+    {
+        private const float Sqrt3 = 1.732050807568877f;
+
+        public void DrawGrid(HexGrid grid, SKCanvas canvas, float cellRadius)
+        {
+            canvas.Clear(SKColors.Black);
+
+            var paint = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = SKColor.Parse("#FFFF00")
+            };
+
+            for (var i = 0; i < grid.Rows; i++)
+                for (var j = 0; j < grid.Columns; j++)
+                {
+                    if (grid.IsAlive(i, j))
+                        DrawCell(canvas, i, j, paint, cellRadius);
+                }
+        }
+
+        private void DrawCell(SKCanvas canvas, int i, int j, SKPaint paint, float cellRadius)
+        {
+            var (x, y) = GetCanvasCoords(i, j, cellRadius);
+            DrawCircle(canvas, x, y, paint, cellRadius);
+            // DrawHexagon(canvas, x, y, paint);
+        }
+
+        private void DrawCircle(SKCanvas canvas, float x, float y, SKPaint paint, float cellRadius) =>
+            canvas.DrawCircle(x, y, cellRadius, paint);
+
+        private void DrawHexagon(SKCanvas canvas, float x, float y, SKPaint paint, float cellRadius)
+        {
+            var path = new SKPath();
+
+            path.MoveTo(x, y - 2 * cellRadius / Sqrt3);
+            path.LineTo(x + cellRadius, y - cellRadius / Sqrt3);
+            path.LineTo(x + cellRadius, y + cellRadius / Sqrt3);
+            path.LineTo(x, y + 2 * cellRadius / Sqrt3);
+            path.LineTo(x - cellRadius, y + cellRadius / Sqrt3);
+            path.LineTo(x - cellRadius, y - cellRadius / Sqrt3);
+            path.Close();
+
+            canvas.DrawPath(path, paint);
+        }
+
+        private (float, float) GetCanvasCoords(int i, int j, float cellRadius)
+        {
+            var x = j * 2 * cellRadius + cellRadius * (i % 2);
+            var y = i * Sqrt3 * cellRadius;
+            return (x, y);
+        }
+    }
+}
--- HexLife.WPF/MainWindow.xaml.cs
+++ HexLife.WPF/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using HexLife.Domain;
+using SkiaSharp.Views.Desktop;
 
 namespace HexLife.WPF
 {
@@ -20,9 +24,45 @@ namespace HexLife.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly HexGridPainter _hexPainter;
+        private float _cellRadius;
+        private HexGameOfLife _gol;
+        private DispatcherTimer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _hexPainter = new HexGridPainter();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(1);
+            _timer.Tick += (o, args) =>
+            {
+                _gol.MakeNextGeneration();
+                Canvas.InvalidateVisual();
+            };
+        }
+
+        private void StartButton_OnClicked(object sender, RoutedEventArgs e)
+        {
+            _cellRadius = Canvas.CanvasSize.Width / 200 / 2;
+            _gol = new HexGameOfLife(200, 200);
+            _gol.ResetToSingleCell();
+            _timer.Start();
+        }
+
+        private void StopButton_OnClicked(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void Canvas_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            if (_gol == null) return;
+
+            var canvas = e.Surface.Canvas;
+            _hexPainter.DrawGrid(_gol.Grid, canvas, _cellRadius);
         }
     }
 }
